10222394d7314506d3e1a0724e6acf89e71c7363
Rishat <42917939+weboson@users.noreply.github.com>
2025-06-04 04:00:10 +0500

    00:34:38 - Reducer, ммутабельность

--- src/store.ts
+++ src/store.ts
@@ -1,35 +1,69 @@
+// теперь много counters: 00:34:38 - Reducer, Иммутабельность
 import { configureStore } from '@reduxjs/toolkit'
 
-type State = {
+type CounterState = {
     counter: number;
 }
 
+export type CounterId = string;
+
+type State = {
+    // TypeScript: Record<keyType, valType> создает объект, с ключем свойств типа CounterId и значением свойств типа CounterState: https://www.typescriptlang.org/docs/handbook/utility-types.html#recordkeys-type 
+    counters: Record<CounterId, CounterState | undefined>;
+}
+
 export type IncrementAction = {
-    type: 'increment'
+    type: 'increment';
+    // чтобы определить какой из множеств имеющихся counter...
+    payload: {
+        counterId: CounterId;
+    }
 }
 
 export type DecrementAction = {
     type: 'decrement'
+    payload: {
+        counterId: CounterId;
+    }
 }
 
 type Action = IncrementAction | DecrementAction
 
+const initialCounterState: CounterState = { counter: 0 }
 const initialState: State = {
-    counter: 0,
+    counters: {},
 }
 
 const reducer = (state = initialState, action: Action): State => {
     switch (action.type) {
-        case "increment":
+        case "increment": {
+            const { counterId } = action.payload;
+            const currentCounter = state.counters[counterId] ?? initialCounterState;
             return {
                 ...state,
-                counter: state.counter + 1,
-            }
-        case "decrement":
+                counters: {
+                    ...state.counters,
+                    [counterId]: {
+                        ...currentCounter,
+                        counter: currentCounter.counter + 1,
+                    }
+                }
+            };
+        }
+        case "decrement": {
+            const { counterId } = action.payload;
+            const currentCounter = state.counters[counterId] ?? initialCounterState;
             return {
                 ...state,
-                counter: state.counter - 1,
-            }
+                counters: {
+                    ...state.counters,
+                    [counterId]: {
+                        ...currentCounter,
+                        counter: currentCounter.counter - 1,
+                    }
+                }
+            };
+        }
         default:
             return state;
     }
